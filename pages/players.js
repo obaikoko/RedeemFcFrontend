25c2b08439fa86b6a59f19a22b04f441493e4d72
obaikoko <jesseobinna7@gmail.com>
2023-04-30 22:17:55 +0100

    initial commit

--- pages/players.js
+++ pages/players.js
@@ -25,31 +25,37 @@ const players = () => {
 
   return (
     <div>
-      <div className={style.card}>
-        <div className={style.title}>
-          <h1>PLAYERS</h1>
+      {players && (
+        <div className={style.card}>
+          <div className={style.title}>
+            <h1>PLAYERS</h1>
+          </div>
+          <div
+            style={{
+              border: '2px solid rgba(0, 0, 0, 0.3)  ',
+              padding: '10px',
+            }}
+          >
+            {players &&
+              players.map((player) => (
+                <Link
+                  className={style.link}
+                  href={'/players/' + player._id}
+                  key={player._id}
+                >
+                  <p> {player.fullName}</p>
+                </Link>
+              ))}
+          </div>
+          <p>
+            If you are player and your name is not listed above kindly register
+            to add your name to the list. <br />
+            Select a player to see player's info.
+          </p>
+          <Link href='/register'>Register</Link>
         </div>
-        <div
-          style={{ border: '2px solid rgba(0, 0, 0, 0.3)  ', padding: '10px' }}
-        >
-          {players &&
-            players.map((player) => (
-              <Link
-                className={style.link}
-                href={'/players/' + player._id}
-                key={player._id}
-              >
-                <p> {player.fullName}</p>
-              </Link>
-            ))}
-        </div>
-        <p>
-          If you are player and your name is not listed above kindly register to
-          add your name to the list. <br />
-          Select a player to see player's info.
-        </p>
-        <Link href='/register'>Register</Link>
-      </div>
+      )}
+      {!players && <p>Loading players...</p>}
     </div>
   );
 };
--- pages/players/[id].js
+++ pages/players/[id].js
@@ -2,72 +2,46 @@ import style from '../../styles/[id].module.css';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
-// export const getStaticPaths = async () => {
-//   const res = await fetch('https://redeemfc.onrender.com/api/users');
-//   const data = await res.json();
-//   const paths = data.map((player) => {
-//     return {
-//       params: { id: player._id.toString() },
-//     };
-//   });
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
-
-// export const getStaticProps = async (context) => {
-//   const id = context.params.id;
-//   const res = await fetch('https://redeemfc.onrender.com/api/users/' + id);
-//   const data = await res.json();
-
-//   return {
-//     props: { player: data },
-//   };
-// };
-
 const Details = () => {
-  const [player, setPlayer] = useState('');
-  const router = useRouter()
-  const {id} = router.query
+  const router = useRouter();
+  const { id } = router.query;
+  const [user, setUser] = useState(null);
+
   useEffect(() => {
-    loadPlayers();
-    // loadPlayersDetails()
-  }, []);
+    const loadDetails = async () => {
+      try {
+        const res = await fetch(
+          `https://redeemfc.onrender.com/api/users/${id}`
+        );
+        const data = await res.json();
+        setUser(data);
+      } catch (error) {
+        console.error(error);
+        setUser(null);
+      }
+    };
 
-  // const loadPlayersDetails = async () => {
-    
-  //   const res = await fetch('https://redeemfc.onrender.com/api/users');
-  //   const data = await res.json();
-  //   console.log(id); 
-  //   // const paths = data.map((player) => {
-    
-  //   // });
-   
-  // };
-
-  const loadPlayers = async () => {
-    console.log(id);
-    const res = await fetch('https://redeemfc.onrender.com/api/users/');
-    const data = await res.json();
-     
-    setPlayer(data);
-    console.log(data);
-  };
+    if (id) {
+      loadDetails();
+    }
+  }, [id]);
 
   return (
     <>
       <div>
-        <div className={style.card}>
-          <h3 className={style.title}>{player.userName}</h3>
-          <div>
-            <h5>Name: {player.fullName}</h5>
-            <p>Email: {player.email}</p>
-            <p>Age: {player.age}</p>
-            <p>State of origin: {player.state}</p>
-            <p>Current Club: {player.club}</p>
+        {user && (
+          <div className={style.card}>
+            <h3 className={style.title}>User Details - {user.name}</h3>
+            <div>
+              <p>Username: {user.username}</p>
+              <p>Email: {user.email}</p>
+              <p>
+                Address: {user.fullName}, {user.state}
+              </p>
+            </div>
           </div>
-        </div>
+        )}
+        {!user && <p>Loading user details...</p>}
       </div>
     </>
   );
